e68556d7ece03b3faa31a3b83431a4a286cf80e4
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2025-06-19 23:40:00 +0100

    Fix: Simplifying syntax for tfrun.nix

--- apps/tfrun.nix
+++ apps/tfrun.nix
@@ -1,35 +1,30 @@
 # This nix function declares multiple nix run profiles that can be used to run terraform commands against this project
-(
-  pkgs:
-  let
-    tfCommands = [
-      "init"
-      "plan"
-      "validate"
-      "apply"
-      "destroy"
-    ];
-    runTfCommand = (
-      src:
-      (command: {
-        type = "app";
-        program = toString (
-          pkgs.writers.writeBash "${command}" ''
+{...}: # workaround for importDir.nix
+{ pkgs, src }:
+let
+  tfCommands = [
+    "init"
+    "plan"
+    "validate"
+    "apply"
+    "destroy"
+  ];
+  runTfCommand = (
+    command: {
+      type = "app";
+      program = toString (
+        pkgs.writers.writeBash "${command}" ''
             set -e
             projectDir="$(git rev-parse --show-toplevel 2>/dev/null || echo "$PWD")"
-              ${pkgs.opentofu}/bin/tofu -chdir="$projectDir/${src}" ${command}
-          ''
-        );
-      })
-    );
-  in
-  (
-    src:
-    builtins.listToAttrs (
-      map (cmd: {
-        name = cmd;
-        value = runTfCommand src cmd;
-      }) tfCommands
-    )
-  )
+          ${pkgs.opentofu}/bin/tofu -chdir="$projectDir/${src}" ${command}
+        ''
+      );
+    }
+  );
+in
+builtins.listToAttrs (
+  map (cmd: {
+    name = cmd;
+    value = runTfCommand cmd;
+  }) tfCommands
 )
